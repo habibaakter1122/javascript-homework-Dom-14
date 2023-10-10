0de83660305f1ab52093bb791d37a6fb2f942660
Habiba Akter <mshabiba573@gmail.com>
2023-10-10 20:41:35 +0600

    complet

---
+++ dom.js
@@ -0,0 +1,14 @@
+
+const byCLASSNAME = document.getElementsByClassName('hello')
+// console.log(byCLASSNAME);
+
+const names = ['monir','mendii','afsha','parvin']
+
+// for (let i=0; i < name.lenghr; i++) {
+//     console.log(name[i]);
+// }
+
+
+for (const name of names) {
+    //  console.log(name);
+}
